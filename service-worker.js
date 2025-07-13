d5a1f74987c7889277e9edd22f4bf0102c3061bb
nerperz <151396783+nerperz@users.noreply.github.com>
2025-07-13 18:04:32 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,31 @@
-const CACHE_NAME = 'excel-automation-cache-v1';
+const CACHE_NAME = 'excel-automation-v1';
+const BASE_PATH = '/MicrosoftExcelAutomation';
+
 const urlsToCache = [
-  '/MicrosoftExcelAutomation/',
-  '/MicrosoftExcelAutomation/index.html',
-  '/MicrosoftExcelAutomation/style.css',
-  '/MicrosoftExcelAutomation/script.js',
-  '/MicrosoftExcelAutomation/manifest.json'
+  `${BASE_PATH}/`,
+  `${BASE_PATH}/index.html`,
+  `${BASE_PATH}/style.css`,
+  `${BASE_PATH}/script.js`,
+  `${BASE_PATH}/manifest.json`,
+  `${BASE_PATH}/icon-192.png`,
+  `${BASE_PATH}/icon-512.png`
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.error('Caching failed', err))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
   );
 });
 
